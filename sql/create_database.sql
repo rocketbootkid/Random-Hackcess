0eae1bf1ff6937d854c9d1806b733a6ffaf182ce
Henry <dittoslash@users.noreply.github.com>
2015-05-31 08:55:04 +0100

    fixed this

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -3,6 +3,8 @@ CREATE DATABASE `hackcess` /*!40100 DEFAULT CHARACTER SET latin1 */;
 CREATE TABLE `journey` (
   `journey_id` int(11) NOT NULL auto_increment,
   `player_id` varchar(45) default NULL,
+  `journey_name` varchar(45) default NULL,
+  `character_id` int(11) default NULL,
   PRIMARY KEY  (`journey_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 

a2262bdfab7929949a9541d327e66bb116761134
Henry <dittoslash@users.noreply.github.com>
2015-05-31 07:05:00 +0100

    Create Character file

---
+++ createcharacter.php
@@ -0,0 +1,10 @@
+<?php
+	include 'functions/debug_functions.php';
+	include 'functions/mysql_functions.php';
+	include 'functions/grid_functions.php';
+	include 'functions/player_functions.php';
+  
+  createCharacter($_POST['name'])
+  echo "<script>window.close</script>"
+  
+?php>

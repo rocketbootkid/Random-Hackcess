ff04771bfd30fcf38602c31feffb5397dbb95686
Henry <dittoslash@users.noreply.github.com>
2015-05-31 08:03:43 +0100

    Update createplayer.php

--- createplayer.php
+++ createplayer.php
@@ -4,7 +4,7 @@
 	include 'functions/grid_functions.php';
 	include 'functions/player_functions.php';
   
-	createPlayer($_POST['name']);
+	createPlayer($_GET['name']);
 	echo "<script>window.close();</script>";
   
 ?>
